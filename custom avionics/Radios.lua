f4b4bf5e69dac74a00339b395c9e17816bdba87d
John Bray <johnbray@Johns-Mac-mini.local>
2019-07-09 23:38:25 +0200

    side panels

--- custom avionics/Radios.lua
+++ custom avionics/Radios.lua
@@ -2,9 +2,9 @@ size={440,256}
 
 function draw() 
 	drawAll(components) 
-	drawText(font_led_24,  30, 232, string.format("%.2f", 0.01*get(COM1) ), 1,1,1)
+	drawText(font_led_24,  30, 232, string.format("%.2f", 0.01*get(COM1) ), 1,0.1,0.1)
 	drawText(font_led_24, 140, 232, string.format("%.2f", get(COM1_SBY_M)+0.001*get(COM1_SBY_k) ), 0.5,0.5,0.5)
-	drawText(font_led_24, 240, 232, string.format("%.2f", 0.01*get(NAV1) ), 1,1,1)
+	drawText(font_led_24, 240, 232, string.format("%.2f", 0.01*get(NAV1) ), 1,0.1,0.1)
 	drawText(font_led_24, 350, 232, string.format("%.2f", get(NAV1_SBY_M)+0.01*get(NAV1_SBY_k) ), 0.5,0.5,0.5)
 	drawText(font_led_24,  40, 200, string.format("%.2f", 0.01*get(COM2) ), 1,0.1,0.1)
 	drawText(font_led_24, 170, 200, string.format("%.2f", get(COM2_SBY_M)+0.001*get(COM2_SBY_k) ), 0.7,0.0,0.0)
